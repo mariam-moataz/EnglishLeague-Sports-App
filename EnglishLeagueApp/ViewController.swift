0c48ef8722038171dd12f9a829f7f65beaf0446d
mariammoataz12 <139216491+mariammoataz12@users.noreply.github.com>
2023-10-11 23:57:47 +0300

    project created

---
+++ EnglishLeagueApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EnglishLeagueApp
+//
+//  Created by Mariam Moataz on 11/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
